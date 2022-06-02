d88d596a827769cf3901e5f97321afdeb1837941
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2022-06-02 13:06:50 +0200

    create java file

---
+++ New Text Document.java
@@ -0,0 +1,6 @@
+public class New Text Document {
+
+    public static void main(String[] args) {
+        System.out.println("njdnjfndskjfhjskdfjksdhfkjsdhfkjshdkjhsdfjkshdkfhsjkdhfkjsdhfkjshdfkjhsdkhfksdhfkjskdhfkjsdhfkjhsdkjhdskjhfjkshdfkjjhsdhfkshdkjfh");
+    }
+}
